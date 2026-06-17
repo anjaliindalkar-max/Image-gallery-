ccb35f17efaf2f893f864ea272e84ef1a1e3f0a1
anjaliindalkar-max <anjaliindalkar@gmail.com>
2026-06-17 22:22:52 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,30 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Image Gallery</title>
-    <link rel="stylesheet" href="style.css">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Responsive Image Gallery</title>
+<link rel="stylesheet" href="style.css">
 </head>
 <body>
 
-<h1>Image Gallery</h1>
+<header>
+    <h1>📸 Image Gallery</h1>
+    <p>Beautiful Responsive Gallery with Lightbox</p>
+</header>
 
 <div class="gallery">
-    <img src="images/img1.jpg" onclick="openModal(this.src)">
-    <img src="images/img2.jpg" onclick="openModal(this.src)">
-    <img src="images/img3.jpg" onclick="openModal(this.src)">
-    <img src="images/img4.jpg" onclick="openModal(this.src)">
-    <img src="images/img5.jpg" onclick="openModal(this.src)">
-    <img src="images/img6.jpg" onclick="openModal(this.src)">
+    <img src="https://picsum.photos/id/1015/600/400" alt="Image 1">
+    <img src="https://picsum.photos/id/1016/600/400" alt="Image 2">
+    <img src="https://picsum.photos/id/1018/600/400" alt="Image 3">
+    <img src="https://picsum.photos/id/1020/600/400" alt="Image 4">
+    <img src="https://picsum.photos/id/1024/600/400" alt="Image 5">
+    <img src="https://picsum.photos/id/1035/600/400" alt="Image 6">
 </div>
 
-<div id="modal" class="modal">
-    <span class="close" onclick="closeModal()">&times;</span>
-    <img id="modalImg">
+<div class="lightbox" id="lightbox">
+    <span class="close">&times;</span>
+    <img class="lightbox-img" src="">
 </div>
 
 <script src="script.js"></script>
-
 </body>
 </html>
